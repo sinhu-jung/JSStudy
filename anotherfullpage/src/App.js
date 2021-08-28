ebf6af3fa75f2de5309a88f678526bd468ddd0ea
sinhu-jung <tlsgn26@naver.com>
2021-08-29 08:10:48 +0900

    fullpage example 함수형으로 변형

--- anotherfullpage/src/App.js
+++ anotherfullpage/src/App.js
@@ -1,80 +1,30 @@
-
-import React from 'react';
-import PropTypes from 'prop-types';
-
+import React, { useRef } from "react";
+import ReactDOM from "react-dom";
 import { FullPage, Slide } from 'react-full-page';
 
-class CustomControls extends React.Component {
-    static propTypes = {
-      className: PropTypes.string,
-      getCurrentSlideIndex: PropTypes.func.isRequired,
-      onNext: PropTypes.func.isRequired,
-      onPrev: PropTypes.func.isRequired,
-      scrollToSlide: PropTypes.func.isRequired,
-      slidesCount: PropTypes.number.isRequired,
-      style: PropTypes.object,
-    }
-  
-    static defaultProps = {
-      className: 'full-page-controls',
-      style: {
-        left: '50%',
-        paddingTop: '10px',
-        position: 'fixed',
-        transform: 'translateX(-50%)',
-      },
-    }
-  
-    renderSlidesNumbers(currentSlideIndex) {
-      const { slidesCount, scrollToSlide } = this.props;
-      const slidesNumbers = [];
-      for (let i = 0; i < slidesCount; i++) {
-        const buttonProps = {
-          disabled: currentSlideIndex === i,
-          key: i,
-          onClick: () => scrollToSlide(i),
-        };
-        slidesNumbers.push(<button type="button" {...buttonProps}>{i + 1}</button>);
-      }
-      return slidesNumbers;
-    }
-  
-    render() {
-      const {
-        getCurrentSlideIndex, slidesCount, style, className,
-      } = this.props;
-      const currentSlideIndex = getCurrentSlideIndex();
-  
-      return (
-        <div className={className} style={style}>
-          <button
-            type="button"
-            disabled={currentSlideIndex === 0}
-            onClick={this.props.onPrev}
-          >
-            previous
-          </button>
-          {this.renderSlidesNumbers(currentSlideIndex)}
-          <button
-            type="button"
-            disabled={currentSlideIndex === slidesCount - 1}
-            onClick={this.props.onNext}
-          >
-            next
-          </button>
-        </div>
-      );
-    }
+export default function App(){
+  const fullPageRef = useRef();
+
+  const onControlsClick = () => {
+    fullPageRef.current.scrollToSlide(0);
   }
-  
-  export default function FullPageExampleCustomControls() {
-    const baseStyle = {
-      alignItems: 'center',
-      display: 'flex',
-      justifyContent: 'center',
-    };
-    return (
-      <FullPage controls={CustomControls}>
+
+  const baseStyle = {
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+  };
+
+  return (
+    <div className="container">
+      <button
+        style={{ position: 'fixed' }}
+        onClick={onControlsClick}
+        type="button"
+      >
+        Controls
+      </button>
+      <FullPage ref={fullPageRef}>
         <Slide
           style={{
             ...baseStyle,
@@ -108,5 +58,6 @@ class CustomControls extends React.Component {
           <h1>4</h1>
         </Slide>
       </FullPage>
-    );
-  }
+    </div>
+  );
+}
