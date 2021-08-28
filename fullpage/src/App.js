dc4b62e536c2a8586b74f6090e0cc9e6589de0d0
sinhu-jung <tlsgn26@naver.com>
2021-08-28 21:04:18 +0900

    react fullpage 연습

---
+++ fullpage/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
+
+import "./styles.css";
+
+export default function App() {
+    return (
+        <ReactFullpage
+        scrollOverflow={true}
+        sectionsColor={["orange", "purple", "green"]}
+        render={({ state, fullpageApi }) => {
+          return (
+            <div id="fullpage-wrapper">
+              <div className="section section1">
+                <h3>Section 1</h3>
+              </div>
+              <div className="section">
+                <div className="slide">
+                  <h3>Slide 2.1</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.2</h3>
+                </div>
+                <div className="slide">
+                  <h3>Slide 2.3</h3>
+                </div>
+              </div>
+              <div className="section">
+                <h3>Section 3</h3>
+                <button onClick={() => fullpageApi.moveTo(1, 0)}>
+                  Move top
+                </button>
+              </div>
+            </div>
+          );
+        }}
+      />
+    );
+}
---
+++ fullpage/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = (env) => {
+    return {
+        mode: 'none',
+        entry: path.resolve(`src/index.js`),
+        output: {
+            path: path.resolve('public'),
+            filename: 'bundle.js',
+            assetModuleFilename: 'assets/images/[hash].[ext]'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.(svg|jpe?g|gif|png|tiff?|bmp|ico|)$/i,
+                type: 'asset/resource'
+            }, {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                        'style-loader', 
+                        { 
+                            loader:'css-loader', 
+                            options: {
+                                modules: true
+                            }
+                        }, 
+                        'sass-loader']
+            }]
+        },
+        devtool: "eval-source-map",
+        devServer: {
+            contentBase: path.resolve('public'),
+            watchContentBase: true,
+            host: "0.0.0.0",
+            port: 9999,
+            inline: true,
+            liveReload: true,
+            hot: false,
+            compress: true,
+            historyApiFallback: true
+        }
+    }; 
+}
