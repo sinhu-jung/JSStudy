ae18f8dbe9327125e12614443612c78c897ea46c
sinhu-jung <tlsgn26@naver.com>
2021-08-29 11:57:03 +0900

    fullpage component forwardref

--- anotherfullpage/src/App.js
+++ anotherfullpage/src/App.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import ReactDOM from "react-dom";
-import { FullPage, Slide } from 'react-full-page';
+import Fullpage from "./Fullpage";
 
 export default function App(){
   const fullPageRef = useRef();
@@ -9,11 +9,6 @@ export default function App(){
     fullPageRef.current.scrollToSlide(0);
   }
 
-  const baseStyle = {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-  };
 
   return (
     <div className="container">
@@ -24,40 +19,7 @@ export default function App(){
       >
         Controls
       </button>
-      <FullPage ref={fullPageRef}>
-        <Slide
-          style={{
-            ...baseStyle,
-            background: '#2ECC40',
-          }}
-        >
-          <h1>Custom Controls</h1>
-        </Slide>
-        <Slide
-          style={{
-            ...baseStyle,
-            background: '#0074D9',
-          }}
-        >
-          <h1>2</h1>
-        </Slide>
-        <Slide
-          style={{
-            ...baseStyle,
-            background: '#00c4ff',
-          }}
-        >
-          <h1>3</h1>
-        </Slide>
-        <Slide
-          style={{
-            ...baseStyle,
-            background: '#d52685',
-          }}
-        >
-          <h1>4</h1>
-        </Slide>
-      </FullPage>
+      <Fullpage ref={fullPageRef}/>
     </div>
   );
 }
