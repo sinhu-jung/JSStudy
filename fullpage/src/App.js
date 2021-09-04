fca07efee31f998632731ffac6d08f29710f83c9
sinhu-jung <tlsgn26@naver.com>
2021-09-04 17:20:29 +0900

    ..

--- fullpage/src/App.js
+++ fullpage/src/App.js
@@ -6,37 +6,37 @@ import ReactFullpage from "@fullpage/react-fullpage";
 import "./styles.css";
 
 export default function App() {
-    return (
-        <ReactFullpage
-        scrollOverflow={true}
-        anchors={["orange", "purple", "green"]}
-        sectionsColor={["orange", "purple", "green"]}
-        render={({ state, fullpageApi }) => {
-          return (
-            <div id="fullpage-wrapper">
-              <div className="section section1">
-                <h3>Section 1<a href="#green">!!!!</a></h3>
+  return (
+    <ReactFullpage
+      scrollOverflow={true}
+      anchors={["orange", "purple", "green"]}
+      sectionsColor={["orange", "purple", "green"]}
+      render={({ state, fullpageApi }) => {
+        return (
+          <div id="fullpage-wrapper">
+            <div className="section section1">
+              <h3>Section 1<a href="#green">!!!!</a></h3>
+            </div>
+            <div className="section">
+              <div className="slide">
+                <h3>Slide 2.1</h3>
               </div>
-              <div className="section">
-                <div className="slide">
-                  <h3>Slide 2.1</h3>
-                </div>
-                <div className="slide">
-                  <h3>Slide 2.2</h3>
-                </div>
-                <div className="slide">
-                  <h3>Slide 2.3</h3>
-                </div>
+              <div className="slide">
+                <h3>Slide 2.2</h3>
               </div>
-              <div className="section">
-                <h3>Section 3</h3>
-                <button onClick={() => fullpageApi.moveTo(1, 0)}>
-                  Move top
-                </button>
+              <div className="slide">
+                <h3>Slide 2.3</h3>
               </div>
             </div>
-          );
-        }}
-      />
-    );
+            <div className="section">
+              <h3>Section 3</h3>
+              <button onClick={() => fullpageApi.moveTo(1, 0)}>
+                Move top
+              </button>
+            </div>
+          </div>
+        );
+      }}
+    />
+  );
 }
